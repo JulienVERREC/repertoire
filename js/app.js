5cf8e433d0579651e63aa5271e437ed55fd32716
JulienVERREC <julien.verrecchia@orange.fr>
2017-03-31 16:32:48 +0200

    inputs renvoyés dans un tableau et numérotés + boucle for

--- js/app.js
+++ js/app.js
@@ -1,34 +1,54 @@
   $(document).ready(function(){
 
-    var annuaire = [];
+      var annuaire = [];
    
-   $("#register").click(function(){
-    
-      var personne = {
-	       "prenom":"",
-	       "nom":"",
-	       "age":""
-         };
+      var personne = {};
 
-    personne["prenom"] = $("#firstname").val();
-    personne["nom"] = $("#name").val();
-	  personne["age"] = $("#old").val();
+        var personne_json = JSON.stringify(personne);
+        sessionStorage.setItem("objet",personne_json);
 
-    console.log(personne);
-    console.log(annuaire);
+        var pesonne_json = sessionStorage.getItem("objet");
+        var pesonne = JSON.parse(personne_json);
+        console.log(personne);
+        
+        $("#register").click(function(){
 
-    annuaire.push(personne);
-    
-    
-    console.log(annuaire);
+            personne = {
+            "prenom":  $("#firstname").val(),
+            "nom":     $("#name").val(),
+            "age":     $("#old").val(),
+            };
 
-    $("input").val("");
-    $("#infoUn").html(personne["prenom"]);
-    $("#infoDeux").html(personne["nom"]);
-    $("#infoTrois").html(personne["age"]);
+      annuaire.push(personne);
+
+
+        $("#ajout").html("");
+
+            for ( var i =  0 ; i < annuaire.length ; i++) {
+        
+            var num = i + 1;
+
+        $("#ajout").append('<tr><td>' + num + '</td><td>' +  annuaire[i].prenom  +  '</td><td>'  
+          + annuaire[i].nom  +  '</td><td>'  +  annuaire[i].age  +  '</td></tr>');
+
+        $("input").val("");
+      
+      };
+
+//      console.log(annuaire);
+//      console.log(personne);
+  });
+
+
+
 });
-});
 
+  
+
+       
+
+
+    
 
 
 
